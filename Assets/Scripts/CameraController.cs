da71f4204ece37755a95b09305d64e0cc1220764
taxi1729 <takorice.02200909@gmail.com>
2025-02-24 17:50:06 +0900

    CameraController追加

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private GameObject Imagawayaki;
+    private Vector3 ImagawaPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Imagawayaki = GameObject.Find("Imagawayaki");
+        ImagawaPos = Imagawayaki.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // targetの移動量分、自分（カメラ）も移動する
+        transform.position += Imagawayaki.transform.position - ImagawaPos;
+        ImagawaPos = Imagawayaki.transform.position;
+
+        // マウスの右クリックを押している間
+        if (Input.GetMouseButton(1))
+        {
+            // マウスの移動量
+            float mouseInputX = Input.GetAxis("Mouse X");
+            // targetの位置のY軸を中心に、回転（公転）する
+            transform.RotateAround(ImagawaPos, Vector3.up, mouseInputX * Time.deltaTime * 200f);
+
+        }
+    }
+}
